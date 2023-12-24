1b4800b6bf3125b22ce73cbbec37228b599e4000
evaldo <evaaldormj@gmail.com>
2023-12-24 12:21:12 -0300

    Tela de edição e Tempdata

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -2,6 +2,7 @@ using GerenciamentoUsuarios.Context.UsuarioContext;
 using GerenciamentoUsuarios.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace GerenciamentoUsuario.Controllers.UsuarioController
 {
@@ -27,8 +28,20 @@ namespace GerenciamentoUsuario.Controllers.UsuarioController
         [HttpPost]
         public ActionResult Cadastrar(Usuario usuario)
         {
+            var usuarios = _context.Usuarios.ToList();
+
             if(ModelState.IsValid)
             {
+
+                foreach(Usuario usuarioModel in usuarios)
+                {
+                    if(usuario.Nome == usuarioModel.Nome)
+                    {
+                        TempData["MensagemCadastro"] = "Um usuario com esse nome ja esta cadastrado! Cadastre um novo.";
+                        return RedirectToAction(nameof(Cadastrar));                        
+                    }
+                }
+
                 _context.Usuarios.Add(usuario);
                 _context.SaveChanges();
 
@@ -38,7 +51,6 @@ namespace GerenciamentoUsuario.Controllers.UsuarioController
             return View();
         }
 
-        [HttpPost]
         public ActionResult Editar(int id)
         {
             var usuario = _context.Usuarios.Find(id);
@@ -48,12 +60,19 @@ namespace GerenciamentoUsuario.Controllers.UsuarioController
                 return NotFound();
             }
 
-            return View(id);
+            return View(usuario);
         }
 
+        [HttpPost]
         public ActionResult Editar(Usuario usuario)
         {
             var usuarioBanco = _context.Usuarios.Find(usuario.ID);
+
+            if(usuario.Senha == null || usuario.Nome == null)
+            {
+                TempData["MensagemEditar"] = "Precisa preencher os campos de Nome e Senha";
+                return RedirectToAction(nameof(Editar));
+            }
 
             usuarioBanco.Nome = usuario.Nome;
             usuarioBanco.Senha = usuario.Senha;
@@ -107,7 +126,9 @@ namespace GerenciamentoUsuario.Controllers.UsuarioController
                     return RedirectToAction(nameof(Listagem));
                 }                
             }
-            return NotFound();
+
+            TempData["Mensagem"] = "Esse usuario/senha nao existe! Cadastre-o para prosseguir.";
+            return RedirectToAction(nameof(Index));
         }       
     }
 }
